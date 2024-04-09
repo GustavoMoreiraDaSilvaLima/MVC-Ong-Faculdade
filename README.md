2c4b4bdbfbf3067455c90e2f7cf36c704eddbb69
DanielAugustoFreire <danielaugustosant@hotmail.com>
2024-04-09 10:20:48 -0300

    zerando o repositorio

--- README.md
+++
@@ -1,4 +0,0 @@
-# MVC-Ong-Faculdade
-Desenvolvimento do aplicativo para ong em js na arquitetura MVC
-# Branch
-Cada um usa uma com seu nome para depois juntar ao final do arquivo. foi criada com o primeiro nome
--- routes/homeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const HomeController = require('../controllers/homeController');
-
-const router = express.Router();
-let ctrl = new HomeController()
-
-router.get("/",ctrl.homeView);
-
-module.exports = router;
