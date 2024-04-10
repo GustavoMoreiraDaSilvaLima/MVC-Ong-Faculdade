91282eb09c679b9c8ec756e6ba249b1f61f7af0c
Gustavo Moreira <gustavomoreira3108@hotmail.com>
2024-04-09 22:35:22 -0300

    feats: Doação acrescentada

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ class HomeController {
 
     //método responsável por devolver o html
     homeView(req, res) {
-        res.render('home', { carros: ["corolla", "fusca", "uno com escada", "del rey"] });
+        res.render('home');
     }
 
     QuemSomosView(req, res) { // OKOKOKOKOKOKOKOKOK
@@ -13,7 +13,18 @@ class HomeController {
     NoticiaView(req, res) {
         res.render('noticias');
     }
-
+    DoacaoView(req, res) {
+        res.render('doacao');
+    }
+    DoaCartaoView(req, res) {
+        res.render('formas_pagamento/cartao', { layout: 'formas_pagamento/cartao' });
+    }
+    DoaBoletoView(req, res) {
+        res.render('formas_pagamento/boleto', { layout: 'formas_pagamento/boleto' });
+    }
+    DoaPixView(req, res) {
+        res.render('formas_pagamento/pix', { layout: 'formas_pagamento/pix' })
+    }
 }
 
 //permite que a classe homeController seja importado
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -6,5 +6,9 @@ let ctrl = new HomeController();
 router.get("/", ctrl.homeView);
 router.get("/QuemSomos", ctrl.QuemSomosView);
 router.get("/noticia", ctrl.NoticiaView);
+router.get("/doacao",ctrl.DoacaoView);
+router.get("/doacao/cartao",ctrl.DoaCartaoView);
+router.get("/doacao/pix",ctrl.DoaPixView);
+router.get("/doacao/boleto",ctrl.DoaBoletoView);
 
 module.exports = router;
