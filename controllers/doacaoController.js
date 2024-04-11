b7b651198da2872edb9053f78e690d28d8250975
Gustavo Moreira <gustavomoreira3108@hotmail.com>
2024-04-10 21:34:40 -0300

    fets:"Fetch 1 -3 "

--- controllers/doacaoController.js
+++ controllers/doacaoController.js
@@ -1,6 +1,6 @@
-const DoacaoModel = require(`../models/doacaoModel`)
+const DoacaoModel = require("../models/doacaoModel");
 
-class doacaoController{
+class doacaoController {
     DoacaoView(req, res) {
         res.render('doacao');
     }
@@ -13,14 +13,113 @@ class doacaoController{
     DoaPixView(req, res) {
         res.render('formas_pagamento/pix', { layout: 'formas_pagamento/pix' });
     }
-    RecebaDoaCartao(req,res){
-        
+    async RecebaDoaCartao(req, res) {
+        let cont = req.body;
+        if (cont.nome != '' && cont.cpf_cnpj != '' && cont.data != '' && cont.opcao1 && cont.selOpcao != 0 && cont.valor > 0 && cont.numcartao != '' && cont.numcvv != '') {
+            let tipocartao = "Cartão" + cont.opcao1;
+            let data = new Date();
+            let status = "APROVADO"
+            let cartao = new DoacaoModel(0, tipocartao, data, cont.nome, cont.valor, status);
+
+            let result = await cartao.doacao_inserir_atualizar();
+            if (result) {
+                res.send({
+                    ok: true,
+                    msg: "Doação feita com sucesso!"
+                });
+            }
+            else {
+                res.send({
+                    ok: false,
+                    msg: "Os serviços estão temporariamente indisponiveis"
+                });
+            }
+        }
+        else {
+            res.send({
+                ok: false,
+                msg: "Parâmetros preenchidos incorretamente!"
+            });
+        }
     }
-    RecebaDoaBoleto(req,res){
+    async RecebaDoaBoleto(req, res) {
+        let status = req.body.status;
+        if (status) {
+            let tipo = "Boleto" 
+            let data = new Date();
+            let boleto = new DoacaoModel(0,tipo,data,null,null,status);
+            
+            let result = await boleto.doacao_inserir_atualizar();
+            if(result) {
+                res.send({
+                    ok: true,
+                    msg: "Doação feita com sucesso!"
+                });
+            }   
+            else{
+                res.send({
+                    ok: false,
+                    msg: "Serviço indisponivel!"
+                });
+            }
+        }
+        else {
+            res.send({
+                ok: false,
+                msg: "Fora de Area"
+            });
+        }
 
     }
-    RecebaDoaPix(req,res){
-
+    async RecebaDoaPix(req, res) {
+        let status = req.body.status;
+        if (status) {
+            let tipo = "Pix" 
+            let data = new Date();
+            let boleto = new DoacaoModel(0,tipo,data,null,null,status);
+            
+            let result = await boleto.doacao_inserir_atualizar();
+            if(result) {
+                res.send({
+                    ok: true,
+                    msg: "Doação feita com sucesso!"
+                });
+            }   
+            else{
+                res.send({
+                    ok: false,
+                    msg: "Serviço indisponivel!"
+                });
+            }
+        }
+        else {
+            res.send({
+                ok: false,
+                msg: "Fora de Area"
+            });
+        }
+    }
+    async RecebaOutraDoa(req,res){
+        cont = req.body;
+        if(cont.txtnome != '' && cont.txtemail && cont.ttelefone && cont.txtcpf && cont.opcao1){
+            let tipo = "Outras";
+            let data = new Date();
+            let status = "Aguardando Aprovação"
+            let outra = new DoacaoModel(0,tipo,data,cont.txtnome,null,status);
+            let result = await outra.doacao_inserir_atualizar();
+            if(result) {
+                res.send({
+                    ok: true,
+                    msg: "Doação feita com sucesso!"
+                });
+            }   
+            else{
+                res.send({
+                    ok: false,
+                    msg: "Serviço indisponivel!"
+                });
+            }
+        }
     }
 }
 
