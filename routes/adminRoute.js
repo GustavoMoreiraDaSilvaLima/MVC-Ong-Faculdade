7a54be7252a8b17b7d2eee2279567a978868658d
ppaulooXD <ph.total12@gmail.com>
2024-04-10 22:23:54 -0300

    feats: ctrl.excluir

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,6 +4,6 @@ const AdminController = require('../controllers/adminController');
 const router = express.Router();
 let ctrl = new AdminController();
 router.get("/doacao",ctrl.ListagemDoacaoView);
-router.post("/doacao/excluir",excluir);
+router.post("/doacao/excluir",ctrl.excluir);
 
 module.exports = router;
