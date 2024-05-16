2f061646f537c7f25832beb30f15e2205cdf8ecc
DanielAugustoSant <80176793+DanielAugustoFreire@users.noreply.github.com>
2024-05-15 23:19:10 -0300

    Update vendaModel.js

--- models/vendaModel.js
+++ models/vendaModel.js
@@ -3,21 +3,27 @@ const Database = require("../utils/database");
 const banco = new Database();
 
 class vendaModel{
-    #cupom_venda //PrimaryKey
-    #id_usu //PrimaryKey
-    #id_prod //PrimaryKey
-    #quantidade_venda // quantidade de venda do produto
-    #valorUni_venda_prod
+    #id_venda
+    #id_usu
+    #id_prod
+    #id_envio
+    #data_envio
+    #data_compra
     #status_venda
+    #quantidade
 
 
-    constructor(cupom_venda, id_usu, id_prod, quantidade_venda, valorUni_venda_prod,status_venda) {
-        this.#cupom_venda = cupom_venda;
-        this.#id_usu = id_usu;
-        this.#id_prod = id_prod;
-        this.#quantidade_venda = quantidade_venda;
-        this.#valorUni_venda_prod = valorUni_venda_prod;
-    }
+constructor(id_venda, id_usu, id_prod, id_envio, data_envio, data_compra, status_venda, quantidade) {
+    this.#id_venda = id_venda;
+    this.#id_usu = id_usu;
+    this.#id_prod = id_prod;
+    this.#id_envio = id_envio;
+    this.#data_envio = data_envio;
+    this.#data_compra = data_compra;
+    this.#status_venda = status_venda;
+    this.#quantidade = quantidade;
+}
+
 
     // Getters
     get CupomVenda() {
