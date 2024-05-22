2a1e74b1596fd0c2fd83cec8972442cb207543bc
GustavoMoreiraDaSilvaLima <136375848+GustavoMoreiraDaSilvaLima@users.noreply.github.com>
2024-05-21 23:36:14 -0300

    Corrigindo Autenticação

--- routes/admin/NoticiaRoute.js
+++ routes/admin/NoticiaRoute.js
@@ -7,12 +7,12 @@ const NoticiaRouter = express.Router();
 
 let ctrl = new NoticiaController()
 
-NoticiaRouter.get('/'/*, autent.NivelPermissaoAdm*/,ctrl.listarNoticias);
-NoticiaRouter.get('/adicionar'/*,autent.NivelPermissaoAdm*/,ctrl.adicionarNoticaView);
-NoticiaRouter.get('/alterar'/*, autent.NivelPermissaoAdm*/,ctrl.editarNoticiaView);
-NoticiaRouter.post('/adicionar'/*, autent.NivelPermissaoAdm*/,ctrl.adicionarNoticia);
-NoticiaRouter.post('/alterar'/*, autent.NivelPermissaoAdm*/,ctrl.editarNoticia);
-NoticiaRouter.post('/excluir'/*, autent.NivelPermissaoAdm*/,ctrl.excluirNoticia);
+NoticiaRouter.get('/', autent.NivelPermissaoAdm,ctrl.listarNoticias);
+NoticiaRouter.get('/adicionar',autent.NivelPermissaoAdm,ctrl.adicionarNoticaView);
+NoticiaRouter.get('/alterar', autent.NivelPermissaoAdm,ctrl.editarNoticiaView);
+NoticiaRouter.post('/adicionar', autent.NivelPermissaoAdm,ctrl.adicionarNoticia);
+NoticiaRouter.post('/alterar', autent.NivelPermissaoAdm,ctrl.editarNoticia);
+NoticiaRouter.post('/excluir', autent.NivelPermissaoAdm,ctrl.excluirNoticia);
 
 
 module.exports = NoticiaRouter;
