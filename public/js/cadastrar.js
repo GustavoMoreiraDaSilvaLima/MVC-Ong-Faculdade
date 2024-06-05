9089ae7c2c5d1d1e237a15ed133d4d51835dcbcb
DanielAugustoFreire <danielaugustosant@hotmail.com>
2024-06-04 22:08:12 -0300

    validacao lado cliente cpf && add file maskara

--- public/js/cadastrar.js
+++ public/js/cadastrar.js
@@ -1,5 +1,4 @@
 
-import {Validador} from './validarCPF.js';
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -13,6 +12,69 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("telefone").style["border-color"] = "#ced4da";
         document.getElementById("sobre_voce").style["border-color"] = "#ced4da";
     }
+
+
+    let cpfIn = document.getElementById("cpf")
+
+    cpfIn.addEventListener("change", () => {
+        if(!validaCPF(cpfIn.value)){
+            cpfIn.style["border-color"] = "red";
+        }else{
+            cpfIn.style["border-color"] = "green";
+        }
+    
+    });
+
+    function validaCPF(cpf) {
+
+        var Soma = 0
+        var Resto
+      
+        var strCPF = String(cpf).replace(/[^\d]/g, '')
+        
+        if (strCPF.length !== 11)
+           return false
+        
+        if ([
+          '00000000000',
+          '11111111111',
+          '22222222222',
+          '33333333333',
+          '44444444444',
+          '55555555555',
+          '66666666666',
+          '77777777777',
+          '88888888888',
+          '99999999999',
+          ].indexOf(strCPF) !== -1)
+          return false
+      
+        for (let i=1; i<=9; i++)
+          Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+      
+        Resto = (Soma * 10) % 11
+      
+        if ((Resto == 10) || (Resto == 11)) 
+          Resto = 0
+      
+        if (Resto != parseInt(strCPF.substring(9, 10)) )
+          return false
+      
+        Soma = 0
+      
+        for (let i = 1; i <= 10; i++)
+          Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+      
+        Resto = (Soma * 10) % 11
+      
+        if ((Resto == 10) || (Resto == 11)) 
+          Resto = 0
+      
+        if (Resto != parseInt(strCPF.substring(10, 11) ) )
+          return false
+      
+        return strCPF
+      }
 
     function cadastrar() {
         limparValidacao();
@@ -30,10 +92,12 @@ document.addEventListener("DOMContentLoaded", function() {
         if(cpf == ""){
             listaErros.push("cpf")
         }else{
+ 
 
-            let validar = new Validador(cpf);
-            if(!validar.Validar_CPF_CNPJ()){
+            if(!validaCPF(cpf)){
                 listaErros.push("cpf"); 
+            }else{
+                cpf = validaCPF(cpf);
             }
 
         }
